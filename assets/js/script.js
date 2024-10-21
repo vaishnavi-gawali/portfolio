60369b6fa2aecfaf802c9c30778a40b5750080de
Vaishnavi Avinash Gawali <145063940+vaishnavi-gawali@users.noreply.github.com>
2024-10-20 23:42:46 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,7 +63,7 @@ document.addEventListener('visibilitychange',
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Vaishnavi's Portfolio";
             $("#favicon").attr("href", "assets/images/favhand.png");
         }
     });
