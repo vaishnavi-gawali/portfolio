9e13a430e2ccfd6eee78ea984d80073c4df9c140
Vaishnavi Avinash Gawali <145063940+vaishnavi-gawali@users.noreply.github.com>
2024-10-21 00:47:27 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Shweta";
+            document.title = "Portfolio | Vaishnavi";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
